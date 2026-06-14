35fd8b04b2db0ecf9be78b4a3867e16cd9f3d5d6
JoelsonJose <joelsonjose222@gmail.com>
2026-06-14 18:23:43 -0300

    config: update api baseURL for local network server address

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_API_URL || 'http://10.0.2.2:8080',
+  baseURL: process.env.EXPO_PUBLIC_API_URL || 'http://192.168.1.9:8080',
   timeout: 30000,
 });
 
